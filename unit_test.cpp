d7667c148465349e370fc6ced4ffbd7d5234b620
jecndlria <jecndlria@gmail.com>
2021-11-23 12:23:32 -0800

    fixed unit_Test.cpp

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,13 +1,5 @@
 #include "gtest/gtest.h"
 
-#include "op_test.hpp"
-#include "rand_test.hpp"
-#include "mult_test.hpp"
-#include "add_test.hpp"
-#include "div_test.hpp"
-#include "pow_test.hpp"
-#include "sub_test.hpp"
-
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
