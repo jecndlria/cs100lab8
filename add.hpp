39ecb1abd8835120516acde4ea1c9d4d386303b2
jecndlria <jecndlria@gmail.com>
2021-11-23 12:35:19 -0800

    implemented new virtual functions to operaters and operand

--- add.hpp
+++ add.hpp
@@ -15,6 +15,11 @@ class Add : public Base
         Add(Base* lhs, Base* rhs) { operands.push_back(lhs); operands.push_back(rhs); }
         virtual double evaluate() { return operands[0]->evaluate() + operands[1]->evaluate(); }
         virtual std::string stringify() { return "(" + operands[0]->stringify() + " + " + operands[1]->stringify() + ")"; }
+        virtual int number_of_children() { return operands.size(); }
+        virtual Base* get_child(int i) {
+            if (i > this->number_of_children()) return nullptr;
+            else return operands[i];
+        }
 };
 
 #endif
